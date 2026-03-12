af507d4b73208ae090e7b6eb516055896860b790
Jeffrey <jeffestaris@gmail.com>
2026-03-12 10:51:29 +0800

    Include lombok dependency.

--- src/main/java/org/eclipse/jakarta/coffee/model/Coffee.java
+++ src/main/java/org/eclipse/jakarta/coffee/model/Coffee.java
@@ -4,34 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
 public class Coffee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String  price;
-
-    //getter and setter
-    // TODO: Use Lombok to generate getter and setter
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPrice() {
-        return price;
-    }
-    public void setPrice(String price) {
-        this.price = price;
-    }
  }
