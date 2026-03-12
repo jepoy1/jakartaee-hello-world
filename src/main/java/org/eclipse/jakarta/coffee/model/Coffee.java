6a28880ac865895a308c09ea3c5c2aa565729c1c
Jeffrey <jeffestaris@gmail.com>
2026-03-12 08:09:24 +0800

    Added Jakarta JPA and Created a drop-and-create hibernate db as mock data.

---
+++ src/main/java/org/eclipse/jakarta/coffee/model/Coffee.java
@@ -0,0 +1,37 @@
+package org.eclipse.jakarta.coffee.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Coffee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String  price;
+
+    //getter and setter
+    // TODO: Use Lombok to generate getter and setter
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+ }
