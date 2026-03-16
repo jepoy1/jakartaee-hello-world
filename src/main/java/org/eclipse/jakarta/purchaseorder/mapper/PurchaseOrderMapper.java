32324951cb264711ffafbcf97273d67f4b3e043e
Jeffrey <jeffestaris@gmail.com>
2026-03-16 09:35:09 +0800

    Remove items as reponsebody property of getPurchaseOrderList

--- src/main/java/org/eclipse/jakarta/purchaseorder/mapper/PurchaseOrderMapper.java
+++ src/main/java/org/eclipse/jakarta/purchaseorder/mapper/PurchaseOrderMapper.java
@@ -2,9 +2,7 @@ package org.eclipse.jakarta.purchaseorder.mapper;
 
 import java.util.List;
 
-import org.eclipse.jakarta.generated.model.PurchaseOrderItemDTO;
 import org.eclipse.jakarta.generated.model.PurchaseOrderDTO;
-import org.eclipse.jakarta.purchaseorder.model.PurchaseOrderItem;
 import org.eclipse.jakarta.purchaseorder.model.PurchaseOrder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -17,10 +15,4 @@ public interface PurchaseOrderMapper {
 
     @Mapping(source = "customer.name", target = "customerName")
     List<PurchaseOrderDTO> toDtoList(List<PurchaseOrder> purchaseOrders);
-
-    @Mapping(source = "product.id", target = "productId")
-    @Mapping(source = "product.productName", target = "productName")
-    PurchaseOrderItemDTO toItemDto(PurchaseOrderItem purchaseOrderItem);
-
-    List<PurchaseOrderItemDTO> toItemDtoList(List<PurchaseOrderItem> purchaseOrderItems);
 }
